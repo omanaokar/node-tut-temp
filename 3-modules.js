1b2b33bc464933616ced3426f7be5c1380c6af21
BuildTools <unconfigured@null.spigotmc.org>
2023-03-10 16:02:06 +0530

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+//Modules
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternativeflava')
+require('./7-mind-grenade')
+
+console.log(data)
+sayHi(names.john)
+sayHi(names.peter)
